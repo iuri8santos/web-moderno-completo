f6c1f9ba4d9a58d0a7a451d4bdecf2971e33b198
Iuri Santos <iurisantos1234@gmail.com>
2022-10-21 15:21:56 -0300

    inicio

---
+++ fundamentos/bloco.js
@@ -0,0 +1,23 @@
+{
+    console.log('Passo 1');
+    {
+        console.log('Passo 1.1');
+
+    }
+    console.log('Passo 2');
+    console.log('Passo 3');
+}
+
+{
+    console.log('Passo 4');
+    {
+        console.log('Passo 4.1');
+        {
+            console.log('Fim do passo 4');
+        }
+    }
+    console.log('Passo 5');
+}
+{
+    console.log('Fim!');
+}
---
+++ fundamentos/comentarios.js
@@ -0,0 +1,12 @@
+console.log('Passo #03');
+console.log('Passo #01');
+// comentário de uma linha
+
+/*
+este 
+é um comentário de
+múltimplas
+linhas
+*/
+
+console.log('Passo #02');
