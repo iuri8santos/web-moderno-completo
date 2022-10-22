8fdd539f2523abd79c4d6ce885823b1680b579a8
Iuri Santos <iurisantos1234@gmail.com>
2022-10-22 17:48:12 -0300

    desafios

---
+++ fundamentos/desafio_area.js
@@ -0,0 +1,9 @@
+const pi = 3.14159
+let raio = 7;
+let areaCirc = pi * raio * raio;
+
+console.log('O valor da área da circunferência é: ' + areaCirc + ' m2');
+
+console.log(Math.PI);
+areaCirc = Math.PI * raio * raio;
+console.log('O valor da área da circunferência é: ' + areaCirc + ' m2');
---
+++ fundamentos/desafio_troca.js
@@ -0,0 +1,10 @@
+let a = 7;
+let b = 94;
+
+// trocar o valor de a por b.
+let temp = a;
+a = b;
+b = temp;
+
+// ou [a, b] = [b, a];
+console.log(a, b);
