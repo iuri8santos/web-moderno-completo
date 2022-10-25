ccf423ce1df0f64471c93f0531dcdfdab792f7b1
Iuri Santos <iurisantos1234@gmail.com>
2022-10-25 04:03:45 -0300

    numeros

---
+++ javascript/fundamentos/tipagemFraca.js
@@ -0,0 +1,21 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.14159
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Evite nomes genéricos e siglas
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso
+
+console.log(valor)
+console.log(typeof valor)
+
+console.log(numero)
+console.log(typeof numero)
+
+console.log(pqp)
+console.log(typeof pqp)
