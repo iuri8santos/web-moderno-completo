a1651689bca2e2124903ba1929c671d41af8239f
Iuri Santos <iurisantos1234@gmail.com>
2022-11-20 02:10:45 -0300

    servidor.js

---
+++ node/projeto/src/servidor.js
@@ -0,0 +1,14 @@
+const porta = 3003
+const express = require('express')
+const app = express()
+
+// app.get('/produtos', (req, res, next) => {
+//    console.log('Middleware 1...')
+//    next()
+// })
+app.get('/produtos', (req, res, next) => {
+    res.send({nome: 'Notebook', preco: 1239.99}) // converter para JSON
+})
+app.listen(porta, () => {
+    console.log(`Servidor está executando na porta ${porta}`)
+})
