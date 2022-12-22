7a09e7e3ee83c7f517e93b847e3b754013d19a58
Iuri Santos <iurisantos1234@gmail.com>
2022-12-22 16:47:27 -0300

    fim ajax :-)

--- ajax/server.js
+++ ajax/server.js
@@ -22,5 +22,20 @@ app.post('/upload', (req, res) => {
         res.end('Concluído com sucesso!')
     })
 })
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+app.get('/parOuImpar', (req, res) => {
+    // req.body
+    // req.query
+    // req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
 // app.get('/teste', (req, res) => res.send(new Date))
-app.listen(8080, () => console.log('Executando...'))
+app.listen(8082, () => console.log('Executando...'))
