cf020d6dddb2848e575f8c62c514d6567eea843b
iuri8santos <108906717+iuri8santos@users.noreply.github.com>
2023-01-02 17:41:23 -0300

    plugins

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,45 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = {
-    mode: 'development',
-    entry: './src/principal.js'
+    mode: modoDev ? 'development' : 'production',
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin({
+                cache: true,
+                parallel: true
+            })
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    ecma: 6,
+                },
+            }),
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader, // Esse conflita com o debaixo
+                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader', // Interpreta @import, url() ...
+                'sass-loader',
+            ]
+        }]
+    }
 }
